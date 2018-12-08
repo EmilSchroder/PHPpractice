41b1413b939e6daa032e302103a1330d41f0eba1
EmilSchroder <eschro66@gmail.com>
2018-12-09 12:49:13 +1300

    loop set up to terminate on E words

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
+
+
 <?php
 
+
+echo "Please type a word \n";
+$letter='z'; 
+
+while($letter!='e'&&$letter!='E'){
+
 $word = trim(fgets(STDIN));
-fwrite(STDOUT, $word."\n");
+$letter = str_split($word)[0];
+
+echo "letter ".$letter."\n";
+echo $word."\n";	
+}
+
+echo "great word, well done! \n"
+
 
 ?>
